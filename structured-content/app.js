77a3a04563437872655a483f71e0dce551af0b37
clettau <chris@Alyssas-MacBook-Air-2.local>
2021-03-13 06:38:39 -0500

    build out file and folder structure

---
+++ structured-content/app.js
@@ -0,0 +1,44 @@
+(function (w, d, ns) {
+    
+    var sendBtn = d.getElementById('sendBtn');
+    var addCardBtn = d.getElementById('addCardBtn');
+    var addMetadataBtn = d.getElementById('addMetadataBtn');
+    var cardOptions = d.getElementById('cardOptions');
+    var clearLogBtn = d.getElementById('clearLogBtn');
+    var increaseFontSizeBtn = d.getElementById('increaseFontSizeBtn');
+    var decreaseFontSizeBtn = d.getElementById('decreaseFontSizeBtn');
+
+    function bindCommand(el, cmd) {
+        el.addEventListener('click', ns.commands[cmd]);
+    }
+
+    function initSdk() {
+        lpTag.agentSDK.init({});
+    }
+
+    function render() {
+        const cardList = [];
+        for (card in ns.data.cards) {
+            cardList.push('<div data-attr-name=' + card + '>' + ns.data.cards[card].title + '</div>');
+        }
+        cardOptions.innerHTML = cardList.join('');
+    }
+
+    function init() {
+        bindCommand(sendBtn, 'sendSC');
+        bindCommand(addCardBtn, 'toggleCardList');
+        bindCommand(cardOptions, 'addCard');
+        bindCommand(addMetadataBtn, 'addMetadata');
+        bindCommand(clearLogBtn, 'clearLog');
+        bindCommand(increaseFontSizeBtn, 'increaseFontSize');
+        bindCommand(decreaseFontSizeBtn, 'decreaseFontSize');
+
+        render();
+
+        initSdk();
+    }
+
+
+    init();
+
+})(window, document, window.structuredContentWidget = window.structuredContentWidget || {});
---
+++ structured-content/lib/commands.js
@@ -0,0 +1,77 @@
+(function (w, d, ns) {
+
+    var html = d.getElementsByTagName('html')[0];
+    var outputLog = d.getElementById('outputLog');
+    var jsonInput = d.getElementById('jsonInput');
+    var addCardBtn = d.getElementById('addCardBtn');
+    var cardOptions = d.getElementById('cardOptions');
+    var metadataInput = d.getElementById('metadataInput');
+
+    function sendSC() {
+        var cmdName = lpTag.agentSDK.cmdNames.writeSC;
+        var data = {
+            json: jsonInput.value,
+            metadata: metadataInput.value
+        };
+
+        clearLog();
+
+        lpTag.agentSDK.command(cmdName, data, function (err) {
+            outputLog.value = err ? err : 'done';
+        });
+    }
+
+    function toggleCardList() {
+        if (cardOptions.style.visibility === 'visible') {
+            hideCardList();
+        } else {
+            cardOptions.style.visibility = 'visible';
+        }
+    }
+
+    function hideCardList() {
+        cardOptions.style.visibility = 'hidden';
+    }
+
+    function addCard(e) {
+        const cardKey = e.target.getAttribute('data-attr-name');
+        jsonInput.value = ns.util.prettyPrint(ns.data.cards[cardKey].content);
+        hideCardList();
+    }
+
+    function addMetadata() {
+        metadataInput.value = ns.util.prettyPrint(ns.data.metadata);
+    }
+
+    function clearLog() {
+        outputLog.value = '';
+    }
+
+    function increaseFontSize() {
+        var fontSize = ns.util.getFontSize(html);
+        ns.util.setFontSize(html, fontSize + 1);
+        adjustToFontSize(fontSize + 1);
+    }
+
+    function decreaseFontSize() {
+        var fontSize = ns.util.getFontSize(html);
+        ns.util.setFontSize(html, fontSize - 1);
+        adjustToFontSize(fontSize - 1);
+    }
+
+    function adjustToFontSize(size) {
+        // cardOptions.style.width = addCardBtn.clientWidth - 1 + 'px';
+        cardOptions.style.top = addCardBtn.clientHeight + 'px';
+    }
+
+    ns.commands = {
+        addCard: addCard,
+        toggleCardList: toggleCardList,
+        addMetadata: addMetadata,
+        sendSC: sendSC,
+        clearLog: clearLog,
+        increaseFontSize: increaseFontSize,
+        decreaseFontSize: decreaseFontSize
+    };
+
+})(window, document, window.structuredContentWidget = window.structuredContentWidget || {});
